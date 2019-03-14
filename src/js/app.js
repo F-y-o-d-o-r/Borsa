bf4fc9bccdfbcf082324e0330b2ecae33223a99c
F-y-o-d-o-r <fyodor.work@gmail.com>
2019-03-14 21:33:40 +0200

    svg4everybody right solution

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,8 @@
 // import sayHello from './lib/sayHello.js';
 import $ from 'jquery';
+import svg4everybody from 'svg4everybody';
 
 // sayHello();
 $(document).ready(function() {
-  /*eslint-disable */
   svg4everybody();
-  /*eslint-enable */
 });
