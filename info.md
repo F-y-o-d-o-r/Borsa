dcbaebcb064baa9ef289614db45375050f694a68
Fyodor <fyodor.work@gmail.com>
2019-03-14 15:13:56 +0200

    init

---
+++ info.md
@@ -0,0 +1,1 @@
+DON'T FORGET to install svg4everybody or svgxuse otherwise IE will not show you svg sprite ¯\_(ツ)_/¯
---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+// import sayHello from './lib/sayHello.js';
+import $ from 'jquery';
+
+// sayHello();
+$(document).ready(function() {
+  /*eslint-disable */
+  svg4everybody();
+  /*eslint-enable */
+});
