d0c276b1e4bbd1e13825137931417557e1fa7e01
Fyodor <fyodor.work@gmail.com>
2019-03-18 17:51:58 +0200

    slider start

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 // import sayHello from './lib/sayHello.js';
 import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
+import './lib/slickSlider';
 
 // sayHello();
 $(document).ready(function() {
